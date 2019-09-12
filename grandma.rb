781df160cf9e49ce861640a4d2cbec79504692d4
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-12 04:47:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
   
 # unless you shout it (type in all capitals).
